0feeb4e41815607aba6a07bef4b76eda0bedc529
My Nguyen <my@ucsb.edu>
2019-10-22 13:44:09 -0700

    small edits to plotPairPotential

--- plotPairPotential.py
+++ plotPairPotential.py
@@ -21,6 +21,17 @@ parser.add_argument("-xl", help = "x label for excluded volume and energy integr
 args = parser.parse_args()
 #  python plotPairPotential.py -i xp0.024_wrapped_CGMap_3_Spline_NoP.pair xp0.024_wrapped_CGMap_4_Spline_NoP.pair xp0.024_wrapped_CGMap_6_Spline_NoP.pair xp0.024_wrapped_CGMap_9_Spline_30knots_cut15_NoP.pair xp0.024_wrapped_CGMap_12_Spline_NoP.pair  -l 3:1 4:1 6:1 9:1 12:1 -n xp0.024 -x 3 4 6 9 12 -xl "Mapping Ratio"
 
+command_args = "-i "
+for file in args.i:
+    command_args += "{} ".format(file)
+command_args += "-l "
+for l in args.l:
+    command_args += "{} ".format(l)
+command_args += "-x "
+for x in args.x:
+    command_args += "{} ".format(x)
+command_args += "-xl \"{}\" ".format(args.xl)    
+command_args += "-n \"{}\" ".format(args.n)
 
 files = args.i
 if not args.x:
@@ -95,6 +106,7 @@ outfile = open('{}_plotPairPotential.log'.format(args.n),'w')
 s = "#{} excludedV Eintegral\n".format(xlabel)
 for i, x in enumerate(xs):
     s += '{x} {v} {e}\n'.format(x=x,v=vs[i],e=es[i])
+s += "ran with arguments:\n{}".format(command_args)
 outfile.write(s)
 outfile.close()
 #plot pair potential
@@ -122,11 +134,14 @@ plt.figure(figsize=[3,2])
 try:
     xs = [float(x) for x in xs]
     plt.plot(xs,vs, 'bo:',markersize = 3, lw = .75, mfc = "None")  
+    plt.plot(xs,np.zeros(len(xs)),'k-', lw = 0.5)
 except:
     plt.plot(range(len(xs)),vs, 'bo:',markersize = 3, lw = .75, mfc = "None")
+    plt.plot(range(len(xs)),np.zeros(len(xs)),'k-', lw = 0.5)
     plt.xticks(np.arange(len(xs)),xs)
 plt.ylabel('Excluded Volume')
 plt.xlabel(xlabel)
+plt.ylim(min(vs)-0.2*abs(min(vs)),max((max(vs)+0.2*abs(max(vs))),20))
 matplotlib.rc('font', size=7)
 matplotlib.rc('axes', titlesize=7)
 plt.rc('legend', fontsize=7)
@@ -138,12 +153,14 @@ plt.figure(figsize=[3,2])
 try:
     xs = [float(x) for x in xs]
     plt.plot(xs,es, 'ro:',markersize = 3, lw = .75, mfc = "None")
+    plt.plot(xs,np.zeros(len(xs)),'k-', lw = 0.5)
 except:
     plt.plot(range(len(xs)),es, 'ro:',markersize = 3, lw = .75, mfc = "None")
+    plt.plot(range(len(xs)),np.zeros(len(xs)),'k-', lw = 0.5)
     plt.xticks(np.arange(len(xs)),xs)
-#plt.plot(range(len(xs)),np.zeros(len(xs)),'k:',lw = .75)
 plt.ylabel('Energy Integral')
 plt.xlabel(xlabel)
+plt.ylim(min(es)-0.2*abs(min(es)),max((max(es)+0.2*abs(max(es))),20))
 matplotlib.rc('font', size=7)
 matplotlib.rc('axes', titlesize=7)
 plt.rc('legend', fontsize=7)
